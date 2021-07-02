6055e7cf118d0d4f9ac8757b8fbb1a1fbd22b5a3
203462 <203462@ids.upchiapas.edu.mx>
2021-07-02 09:11:54 -0500

    cambios generales

--- ArbolBinario.java
+++ ArbolBinario.java
@@ -2,49 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class ArbolBinario {
-    public static Scanner entrada = new Scanner (System.in);    
+    public static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
         int opc, valor;
-        
-        
-        Nodo raiz= new Nodo();
-        raiz=crearArbol(raiz);
-        Recorridos recorrido1 =new Recorridos();
+        Nodo raiz = new Nodo();
+        raiz = crearArbol(raiz);
         do {
-            System.out.println("1. Agregar productos\n2. Eliminar\n3. Vender\n4. Mostrar Productos\n5. Guardar y Salir");        
-            opc=entrada.nextInt();
-            switch (opc){
-                case 1: 
+            System.out
+                    .println("1. Agregar productos\n2. Eliminar\n3. Vender\n4. Mostrar Productos\n5. Guardar y Salir");
+            opc = entrada.nextInt();
+            switch (opc) {
+                case 1:
                     agregarProducto(raiz);
                     break;
                 case 2:
                     System.out.println("introduzca el id del producto a eliminar");
-                    valor=entrada.nextInt();
+                    valor = entrada.nextInt();
                     eliminar(raiz, valor);
-                break;
-                case 3:
-
-                break;
-                
-                case 4:System.out.println("Total: "+totalNodos(raiz)+" nodos");
-                    System.out.println("Total nodos hoja: "+totalHojas(raiz)+" nodo (s)");
                     break;
-                case 5: System.out.println("Recorrido pre-orden\n");
-                Recorridos.preOrden(raiz);
-                System.out.println("Recorrido post-orden\n");
-                Recorridos.postOrden(raiz);
-                System.out.println("Recorrido inOrden\n");
-                Recorridos.inOrden(raiz);
-                break;
-                case 6:
-                    System.out.println("Ingrese el valor a buscar: ");
-                    int valorBuscar = entrada.nextInt();
-                    buscarNodo(raiz, valorBuscar);
-                }
-        }while(opc<5);   
+                case 3:
+                    raiz = vender(raiz);
+                    break;
+
+                case 4:
+                    System.out.println("Recorrido inOrden\n");
+                    Recorridos.inOrden(raiz);
+                    break;
+                case 5:
+                    escribir(raiz);
+
+                    break;
+            }
+        } while (opc < 6);
     }
-    
-    public static Nodo crearArbol (Nodo nodoPadre){        
+
+    public static Nodo crearArbol(Nodo nodoPadre) {
         int id, cantidad;
         Double precio;
         String nombre;
@@ -52,22 +45,20 @@ public class ArbolBinario {
             String texto;
             String[] partes;
             int contador = 0;
-
             FileReader archivo = new FileReader("Productos.txt");
             BufferedReader contenedor = new BufferedReader(archivo);
-
-            while((texto=contenedor.readLine()) != null){
-                partes = texto.split("-",4);
+            while ((texto = contenedor.readLine()) != null) {
+                partes = texto.split("-", 4);
                 id = Integer.parseInt(partes[0]);
                 nombre = partes[1];
                 cantidad = Integer.parseInt(partes[2]);
                 precio = Double.parseDouble(partes[3]);
-                if(contador==0){
+                if (contador == 0) {
                     nodoPadre.actualizar(id, nombre, cantidad, precio);
-                }else{
+                } else {
                     insertarNodo(nodoPadre, id, nombre, cantidad, precio);
                 }
-                contador ++;
+                contador++;
             }
             contenedor.close();
         } catch (FileNotFoundException e) {
@@ -75,117 +66,98 @@ public class ArbolBinario {
         } catch (IOException e) {
             System.out.println("Error");
         }
-        return nodoPadre;          
+        return nodoPadre;
     }
-    
+
     private static void agregarProducto(Nodo raiz) {
         int id, cantidad;
         String nombre;
         Double precio;
-
-        System.out.println("id: ");
+        System.out.println("Ingrese el id: ");
         id = entrada.nextInt();
-        System.out.println("Nombre: ");
+        System.out.println("Ingrese el nombre: ");
         entrada.nextLine();
         nombre = entrada.nextLine();
-        System.out.println("Precio: ");
+        System.out.println("Ingrese el precio: ");
         precio = entrada.nextDouble();
-        System.out.println("Cantidad: ");
+        System.out.println("Ingrese la cantidad: ");
         cantidad = entrada.nextInt();
         int validar = buscarNodo(raiz, id);
-        if(validar == 0){
+        if (validar == 0) {
             insertarNodo(raiz, id, nombre, cantidad, precio);
-            System.out.println("Producto aniadido");
+            System.out.println("El producto ha sido añadido");
         }
-
     }
 
     private static Nodo eliminar(Nodo raiz, int id) {
         boolean bandera;
-        Nodo otroNodo=new Nodo();
-        Nodo aux=new Nodo();
-        Nodo aux1=new Nodo();
-        if (raiz!=null){
-            if (id < raiz.getId()){
+        Nodo otroNodo = new Nodo();
+        Nodo aux = new Nodo();
+        Nodo aux1 = new Nodo();
+        if (raiz != null) {
+            if (id < raiz.getId()) {
                 raiz.setIzq(eliminar(raiz.getIzq(), id));
-            }else{
-                if (id > raiz.getId()){
+            } else {
+                if (id > raiz.getId()) {
                     raiz.setDer(eliminar(raiz.getDer(), id));
-                }else{                      
-                    otroNodo=raiz;
-                    if(otroNodo.getDer()==null){
-                        raiz=otroNodo.getIzq();
-                    }else{
-                        if(otroNodo.getIzq()==null){
-                            raiz=otroNodo.getDer();
-                        }else{
-                            aux=raiz.getIzq();
-                            bandera=false;
-                            while (aux.getDer()!=null){
-                                aux1=aux;
-                                aux=aux.getDer();
-                                bandera=true;
+                } else {
+                    otroNodo = raiz;
+                    if (otroNodo.getDer() == null) {
+                        raiz = otroNodo.getIzq();
+                    } else {
+                        if (otroNodo.getIzq() == null) {
+                            raiz = otroNodo.getDer();
+                        } else {
+                            aux = raiz.getIzq();
+                            bandera = false;
+                            while (aux.getDer() != null) {
+                                aux1 = aux;
+                                aux = aux.getDer();
+                                bandera = true;
                             }
-                            raiz.setInfo(aux,raiz);
-                            otroNodo=aux;
-                            if (bandera==true){
+                            raiz.setInfo(aux, raiz);
+                            otroNodo = aux;
+                            if (bandera == true) {
                                 aux1.setDer(aux.getIzq());
-                            }else{
+                            } else {
                                 raiz.setIzq(aux.getIzq());
                             }
                         }
                     }
-                    otroNodo=null;
+                    otroNodo = null;
                 }
             }
-        }else{
-            System.out.println("La informacion no se encuentra");
-        }   
+        } else {
+            System.out.println("La información no se ha encontrado");
+        }
         return raiz;
     }
 
     public static void insertarNodo(Nodo nodoPadre, int id, String nombre, int cantidad, Double precio) {
         if (id < nodoPadre.getId()) {
             if (nodoPadre.getIzq() == null) {
-                Nodo nuevoNodo = new Nodo(id, nombre,cantidad, precio);
+                Nodo nuevoNodo = new Nodo(id, nombre, cantidad, precio);
                 nodoPadre.setIzq(nuevoNodo);
             } else {
-                insertarNodo(nodoPadre.getIzq(), id, nombre,cantidad, precio);
+                insertarNodo(nodoPadre.getIzq(), id, nombre, cantidad, precio);
             }
         } else {
-            if(id > nodoPadre.getId()){
+            if (id > nodoPadre.getId()) {
                 if (nodoPadre.getDer() == null) {
-                    Nodo nuevoNodo = new Nodo (id, nombre,cantidad, precio);
+                    Nodo nuevoNodo = new Nodo(id, nombre, cantidad, precio);
                     nodoPadre.setDer(nuevoNodo);
                 } else {
-                    insertarNodo(nodoPadre.getDer(), id, nombre,cantidad, precio);
+                    insertarNodo(nodoPadre.getDer(), id, nombre, cantidad, precio);
                 }
             }
         }
     }
 
-    public static int totalNodos(Nodo raiz){
-        if (raiz != null)
-            return 1 + totalNodos(raiz.getIzq()) +totalNodos(raiz.getDer());
-        else
-            return 0;
-    }
-    public static int totalHojas(Nodo raiz){
-        if (raiz != null){
-            if(raiz.getIzq()==null && raiz.getDer()==null){//Es un nodo hoja
-               return 1;
-            }
-            else{
-                return totalHojas(raiz.getIzq())+totalHojas(raiz.getDer()) ;
-            }
-        }
-        else return 0;
-    }
     private static int buscarNodo(Nodo raiz, int valorBuscar) {
-        int  op=0;
+        int op = 0;
         if (valorBuscar < raiz.getId()) {
             if (raiz.getIzq() == null) {
-                op=0;
+                op = 0;
             } else {
                 buscarNodo(raiz.getIzq(), valorBuscar);
             }
@@ -193,40 +165,62 @@ public class ArbolBinario {
             if (valorBuscar > raiz.getId()) {
                 if (raiz.getDer() == null) {
                     System.out.println("Valor no encontrado");
-                    op=0;
+                    op = 0;
                 } else {
                     buscarNodo(raiz.getDer(), valorBuscar);
                 }
             } else {
-                op=1;
+                op = 1;
                 System.out.println("Valor encontrado: " + raiz.getId());
             }
         }
         return op;
 
     }
+
     private static Nodo vender(Nodo raiz) {
         int id, cantidad;
         System.out.println("Ingrese el id del producto a vender: ");
         id = entrada.nextInt();
         System.out.println("Cantidad a vender: ");
         cantidad = entrada.nextInt();
-        Nodo nodo= new Nodo(id, "", cantidad, 0.0);
+        Nodo nodo = new Nodo(id, "", cantidad, 0.0);
         int validar = buscarNodo(raiz, nodo.getId());
-        if(validar != 0){
-            if((nodo.getCantidad() <= raiz.getCantidad()) && (nodo.getCantidad() > 0)){
+        if (validar != 0) {
+            if ((nodo.getCantidad() <= raiz.getCantidad()) && (nodo.getCantidad() > 0)) {
                 raiz.setCantidad(raiz.getCantidad() - nodo.getCantidad());
-                if(raiz.getCantidad() == 0){
-                    System.out.println("El producto sera eliminado ya que no tiene existencias");
+                if (raiz.getCantidad() == 0) {
+                    System.out.println("Producto sin unidades, este producto será eliminado");
                     raiz = eliminar(raiz, raiz.getId());
-                }else{
+                } else {
                     System.out.println("Existencias actualizadas");
                 }
-            }else{
-                System.out.println("Productos insuficientes");
+            } else {
+                System.out.println("El producto elegido tiene unidades insuficientes");
             }
-            System.out.println("Accion exitosa");
-        }     
-        return raiz; 
+            System.out.println("La venta se completo");
+        }
+        return raiz;
+    }
+
+    private static void escribirArchivo(Nodo raiz, BufferedWriter bw) throws IOException {
+        if (raiz != null) {
+            escribirArchivo(raiz.getIzq(), bw);
+            bw.write(raiz.getInfo(raiz).getId() + "-" + raiz.getInfo(raiz).getNombre() + "-"
+                    + raiz.getInfo(raiz).getCantidad() + "-" + raiz.getInfo(raiz).getPrecio() + "\n");
+            escribirArchivo(raiz.getDer(), bw);
+        }
+    }
+
+    public static void escribir(Nodo raiz) {
+        BufferedWriter bw;
+        try {
+            File archivo = new File("Productos.txt");
+            bw = new BufferedWriter(new FileWriter(archivo, false));
+            escribirArchivo(raiz, bw);
+            bw.close();
+        } catch (IOException e) {
+            System.out.println("No se pudo escribir dentro del archivo");
+        }
     }
 }
--- Nodo.java
+++ Nodo.java
@@ -1,68 +1,80 @@
 
 public class Nodo {
-    //Componentes de un Nodo
     private int id;
     private String nombre;
     private int cantidad;
     private Double precio;
 
-    private Nodo izq;//Para enlace por la izquierda
-    private Nodo der;//Para enlace por la derecha
-    
-    //Constructores del Nodo
-    public Nodo (){    }
+    private Nodo izq;
+    private Nodo der;
+
+    public Nodo() {
+    }
 
     public Nodo(int id2, String nombre2, int cantidad2, Double precio2) {
     }
 
     public void actualizar(int id2, String nombre2, int cantidad2, Double precio2) {
-        id= id2;
-        nombre=nombre2;
-        cantidad=cantidad2;
-        precio=precio2;
+        id = id2;
+        nombre = nombre2;
+        cantidad = cantidad2;
+        precio = precio2;
     }
-    public Nodo getInfo(Nodo nodo){
+
+    public Nodo getInfo(Nodo nodo) {
         return nodo;
     }
-    public void setInfo(Nodo nodo1, Nodo raiz){
-        raiz=nodo1;
+
+    public void setInfo(Nodo nodo1, Nodo raiz) {
+        raiz = nodo1;
     }
-    //Metodos get y set para manipulaci�n de Nodos
-    public Nodo getIzq(){
+
+    public Nodo getIzq() {
         return izq;
     }
-    public Nodo getDer(){
+
+    public Nodo getDer() {
         return der;
     }
-    public void setIzq(Nodo nodo){
-        izq=nodo;
+
+    public void setIzq(Nodo nodo) {
+        izq = nodo;
     }
-    public void setDer(Nodo nodo){
-        der=nodo;
+
+    public void setDer(Nodo nodo) {
+        der = nodo;
     }
+
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
+
     public int getCantidad() {
         return cantidad;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public int getId() {
         return id;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
+
     public Double getPrecio() {
         return precio;
     }
-    
+
 }
